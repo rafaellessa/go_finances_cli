229671e1012a484d0edb26e71e1adbb1419780b5
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-05 20:03:41 -0300

    feat: pass props to highlight component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -9,9 +9,24 @@ const Dashboard: React.FC = () => {
     <Container>
       <Header onPress={() => {}} />
       <HighLightCards>
-        <HighLightCard />
-        <HighLightCard />
-        <HighLightCard />
+        <HighLightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighLightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 7.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighLightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
       </HighLightCards>
     </Container>
   );
