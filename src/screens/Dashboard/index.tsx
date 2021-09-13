0b7919e82dd92eaa0843471de5af64fb1b097190
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-13 19:15:11 -0300

    feat: implements transactioncard into dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import Header from '../../components/Header';
 import HighLightCard from '../../components/HighLightCard';
+import TransactionCard from '../../components/TransactionCard';
 
-import {Container, HighLightCards} from './styles';
+import {
+  Container,
+  HighLightCards,
+  Transactions,
+  Title,
+  TransactionsList,
+} from './styles';
 
 const Dashboard: React.FC = () => {
+  const data = [
+    {
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign',
+      },
+      date: '13/09/2021',
+    },
+    {
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign',
+      },
+      date: '13/09/2021',
+    },
+    {
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign',
+      },
+      date: '13/09/2021',
+    },
+  ];
+
   return (
     <Container>
       <Header onPress={() => {}} />
@@ -28,6 +65,14 @@ const Dashboard: React.FC = () => {
           lastTransaction="Última entrada dia 13 de abril"
         />
       </HighLightCards>
+
+      <Transactions>
+        <Title>Movimentações</Title>
+        <TransactionsList
+          data={data}
+          renderItem={({item}) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 };
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,4 +1,5 @@
-import {RFPercentage} from 'react-native-responsive-fontsize';
+import {FlatList} from 'react-native';
+import {RFPercentage, RFValue} from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -15,3 +16,17 @@ export const HighLightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+  margin-top: ${RFPercentage(8)}px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({theme}) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+  margin-bottom: 16px;
+`;
+
+export const TransactionsList = styled.FlatList`` as unknown as typeof FlatList;
